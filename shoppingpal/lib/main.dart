6509f3c4ede2114e216d169b557ca8304ae57f32
CristiTurcan <turcancristian02@gmail.com>
2023-05-23 20:18:04 +0300

    Added bottom navigation bar.

--- shoppingpal/lib/main.dart
+++ shoppingpal/lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shoppingpal/pages/main_page.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(255, 140, 0, 1),
+        title: const Center(child: Text('ShoppingPal')),
+      ),
+      body: MainPage(),
+    ));
+  }
+}
