f05bce37864a308f6ae3e5f2f7ab07380b09f4ff
CristiTurcan <turcancristian02@gmail.com>
2023-05-23 22:41:31 +0300

    Implemented UI for account tab.

--- shoppingpal/lib/pages/account.dart
+++ shoppingpal/lib/pages/account.dart
@@ -6,7 +6,50 @@ class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text('Account Page')),
-    );
+        child: Center(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 70,
+          ),
+          Container(
+            decoration: const BoxDecoration(
+                border: Border(bottom: BorderSide(width: 0.5))),
+            child: const Center(
+              child: Column(
+                children: [
+                  Icon(
+                    Icons.account_circle_sharp,
+                    size: 100,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Text('etrusc1 - parola de la db',
+                      style:
+                          TextStyle(fontSize: 21, fontWeight: FontWeight.bold)),
+                  SizedBox(
+                    height: 40,
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 300,
+          ),
+          ElevatedButton(
+              onPressed: () => {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(220, 121, 0, 1),
+                  fixedSize: const Size(150, 50),
+                  textStyle: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w500),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20))),
+              child: const Text('Sign Out')),
+        ],
+      ),
+    ));
   }
 }
