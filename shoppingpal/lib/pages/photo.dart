a93755548d5c55a0c8fe4ce50a72c2475cb1e7f6
CristiTurcan <turcancristian02@gmail.com>
2023-05-23 21:38:04 +0300

    Added button for image functionality.

--- shoppingpal/lib/pages/photo.dart
+++ shoppingpal/lib/pages/photo.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
-class PhotoPage extends StatelessWidget {
+class PhotoPage extends StatefulWidget {
   const PhotoPage({super.key});
 
   @override
+  State<PhotoPage> createState() => _PhotoPageState();
+}
+
+class _PhotoPageState extends State<PhotoPage> {
+  @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text("Photo Page")),
+      child: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 250,
+              width: 250,
+            ),
+            CustomButton(title: 'Insert Image', onClick: () => {}),
+            const SizedBox(height: 15),
+            CustomButton(title: 'Take Photo', onClick: () => {})
+          ],
+        ),
+      ),
     );
   }
+}
+
+Widget CustomButton({
+  required String title,
+  required VoidCallback onClick,
+}) {
+  return Container(
+    child: ElevatedButton(
+      onPressed: onClick,
+      style: ElevatedButton.styleFrom(
+          backgroundColor: Color.fromRGBO(220, 121, 0, 1),
+          fixedSize: const Size(150, 50),
+          textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))),
+      child: Text(title),
+    ),
+  );
 }
