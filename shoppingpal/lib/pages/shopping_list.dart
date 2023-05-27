e99ecf9db7c4a0bd6a9ce1e0651fa34edc07e6cd
CristiTurcan <turcancristian02@gmail.com>
2023-05-27 18:39:13 +0300

    Implemented receipt list.

--- shoppingpal/lib/pages/shopping_list.dart
+++ shoppingpal/lib/pages/shopping_list.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:shoppingpal/pages/getUserID.dart';
+import 'package:shoppingpal/pages/preview.dart';
 
 class ShoppingListPage extends StatefulWidget {
   const ShoppingListPage({super.key});
@@ -9,17 +13,101 @@ class ShoppingListPage extends StatefulWidget {
 }
 
 class _ShoppingListPageState extends State<ShoppingListPage> {
+  static String receiptsResponse = '''{
+  "receipts": [
+    {
+      "id": 5,
+      "total": 39.83,
+      "user_id": "BimtoTlu2IMu8zI0gv9CScJtNa82",
+      "name": "Nume Casier",
+      "shop_name": "Nume Casier",
+      "date": "2023-05-27T17:48:09.244567"
+    },
+    {
+      "id": 1,
+      "total": 123.45,
+      "user_id": "BimtoTlu2IMu8zI0gv9CScJtNa82",
+      "name": "test_merchant",
+      "shop_name": "test_merchant",
+      "date": "2023-05-27T05:32:03.345332"
+    },
+    {
+      "id": 6,
+      "total": 39.83,
+      "user_id": "BimtoTlu2IMu8zI0gv9CScJtNa82",
+      "name": "Nume Casier",
+      "shop_name": "Nume Casier",
+      "date": "2023-05-27T17:56:30.937540"
+    },
+    {
+      "id": 7,
+      "total": 39.83,
+      "user_id": "BimtoTlu2IMu8zI0gv9CScJtNa82",
+      "name": "Nume Casier",
+      "shop_name": "Nume Casier",
+      "date": "2023-05-27T17:58:58.268098"
+    }
+  ]
+}''';
+
+  static final receiptsJSON = jsonDecode(receiptsResponse);
+
+  List receipts = receiptsJSON["receipts"];
+
   @override
   void initState() {
     getUserID.getUID();
     getUserID.getUserEmail();
     super.initState();
+    print(receiptsJSON);
+    print(receipts.length);
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Center(child: Text("Shopping List")),
+      child: ListView.builder(
+        itemCount: receipts.length,
+        itemBuilder: (context, index) {
+          return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              key: ValueKey(receipts[index]["id"]),
+              margin: const EdgeInsets.all(8),
+              color: const Color.fromARGB(255, 255, 164, 53),
+              child: Column(
+                children: [
+                  CustomListButton(
+                    index: index,
+                    receipts: receipts,
+                    shop_name: "shop_name",
+                    date: "date",
+                    total: "total",
+                  ),
+                ],
+              ));
+        },
+      ),
     );
   }
+}
+
+Widget CustomListButton({
+  required index,
+  required List receipts,
+  required String shop_name,
+  required String date,
+  required String total,
+}) {
+  return ElevatedButton(
+    onPressed: () => {},
+    child: Column(
+      children: [
+        Text('Shop Name: ${receipts[index][shop_name]}'),
+        Text('Date: ${receipts[index][date]}'),
+        Text('Total: ${receipts[index][total]}'),
+      ],
+    ),
+  );
 }
--- shoppingpal/lib/services/save_receipt.dart
+++ shoppingpal/lib/services/save_receipt.dart
@@ -1,8 +1,7 @@
 import 'package:http/http.dart' as http;
 
 Future<http.Response> saveReceipt(String userId, String jsonData) async {
-  var url =
-      Uri.parse('http://192.168.2.139:8000/save-receipt/?user_id=$userId');
+  var url = Uri.parse('http://172.20.10.2:8000/save-receipt/?user_id=$userId');
 
   var response = await http.post(
     url,
