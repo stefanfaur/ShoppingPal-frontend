7c2741651b95d3a05882fcbbae59c103688dff5c
CristiTurcan <turcancristian02@gmail.com>
2023-05-27 20:14:49 +0300

    Implemented item list for each receipt.

--- shoppingpal/lib/services/get_receipt_info.dart
+++ shoppingpal/lib/services/get_receipt_info.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<http.Response> getReceiptInfo(String receiptId) async {
+Future<Map<String, dynamic>> getReceiptInfo(String receiptId) async {
   var url = Uri.parse('http://192.168.2.139:8000/receipts/$receiptId/items');
 
   var response = await http.get(url);
